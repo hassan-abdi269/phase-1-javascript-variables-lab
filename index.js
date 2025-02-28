4a0e4dfdeaf8593e610f72532b350d9ac6a321a0
hassan-abdi269 <honze2030@gamail.com>
2025-02-28 08:57:03 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName = 'Scuber';  
+const companyLocation = 'New York';  
+
+let companyRevenue = 1000000; 
+
+let mostProfitableNeighborhood = 'Chelsea'; 
+
+let companyCeo = 'Susan Smith';
